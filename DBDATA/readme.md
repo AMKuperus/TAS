ebfce11fd23e5d7c836c81c2c24ed29a8eb18fc8
AMKuperus <jezeewolde@gmail.com>
2017-05-19 13:36:19 +0200

    TAS Database
    + readme.md to explain database file
    + database import-file

---
+++ DBDATA/readme.md
@@ -0,0 +1,10 @@
+# Database import
+File for importing the database for TAS.
+
+## Users
+Premade users student monitor and admin can login with password: 1234567
+Those users have their roles predefined so you can start to test the system.
+If you would like to start fresh without any users you should setup a monitor-user and a admin-user by making their roles MONITOR or ADMIN, after registration, in the database.
+
+## installation
+Simply import the tas.sql into your phpmyadmin and you should be good to go.
---
+++ DBDATA/tas.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: May 19, 2017 at 04:25 AM
+-- Server version: 5.7.17
+-- PHP Version: 7.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tas`
+--
+CREATE DATABASE IF NOT EXISTS `tas` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `tas`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+CREATE TABLE `activity` (
+  `activityId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `activity` varchar(255) NOT NULL,
+  `startDate` date DEFAULT NULL,
+  `endDate` datetime DEFAULT NULL,
+  `timeSpent` int(2) DEFAULT NULL,
+  `difficulty` int(2) NOT NULL,
+  `satisfaction` int(2) NOT NULL,
+  `notes` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE `group` (
+  `index` int(11) NOT NULL,
+  `group` varchar(15) NOT NULL DEFAULT 'UNIQUE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` char(60) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `role` varchar(255) NOT NULL,
+  `group` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `username`, `password`, `email`, `firstname`, `lastname`, `role`, `group`) VALUES
+(1, 'student', '$2y$10$Y9s2g/.9XFzY4qL29pEH6eM8YdHOXbmmeO4tLYbR/gytfCmgxy4cq', 'example@example.com', '', '', 'STUDENT', ''),
+(2, 'monitor', '$2y$10$Vjg4vnElXuQ6AiQ4aqPHie0YljFwxCCFHArMlarBHPB54pY/jvcIG', 'example@example.com', '', '', 'MONITOR', ''),
+(3, 'admin', '$2y$10$59ai0n/9o5yMhSBW6Cs9f.Nq7Nxqz2knuSTbAeS2onWbSur5pUT.q', 'example@example.com', '', '', 'ADMIN', ''),
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`activityId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`index`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `activityId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `group`
+--
+ALTER TABLE `group`
+  MODIFY `index` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activity`
+--
+ALTER TABLE `activity`
+  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
