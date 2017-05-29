98d951c6d0633f542854d92dcf863e5b15698358
AMKuperus <jezeewolde@gmail.com>
2017-05-29 11:47:03 +0200

    TAS
    + changed route removed homepage so user gets directly to sign in and
    gets welcomed after signing in

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,13 +5,18 @@ namespace App\Presenters;
 use Nette;
 use App\Model;
 
+use Nette\Mail\Message;
+use Nette\Mail\SmtpMailer;
 
 class HomepagePresenter extends BasePresenter
 {
 	private $database;
 
-	public function __construct(Nette\Database\Context $database) {
+	private $mailer;
+
+	public function __construct(Nette\Database\Context $database, Nette\Mail\IMailer $mailer) {
 		$this->database = $database;
+		$this->mailer = $mailer;
 	}
 
 	public function renderUsername() {
@@ -20,7 +25,26 @@ class HomepagePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		#$this->template->anyVariable = 'any value';
+		$this->redirect('Sign:in');
+	}
+
+
+
+	public function renderSendEmail() {
+		$mail = new Message;
+		$mail->setFrom('Someone <test@amkuperus.nl>')
+					->addTo('jezeewolde@gmail.com')
+					->setSubject('Testemail')
+					->setBody('Testing123, de mailfunctie blijkt te werken!');
+
+		try {
+			$this->mailer->send($mail);
+			$this->flashMessage('Mail send succesfull.');
+		} catch (SmtpException $e) {
+			$this->flashMessage($e->getMessage());
+		}
+		$this->redirect('Homepage:');
 	}
 
 }
--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -22,7 +22,7 @@ class SignPresenter extends BasePresenter
 	protected function createComponentSignInForm()
 	{
 		return $this->signInFactory->create(function () {
-			$this->redirect('Homepage:');
+			$this->redirect('Homepage:welcome');
 		});
 	}
 
